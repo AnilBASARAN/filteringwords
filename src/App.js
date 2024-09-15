7315a397e940b9cc35aba8335579d207fd5fef05
Anıl Başaran <nlbsrn@gmail.com>
2024-09-16 02:38:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import words from "./words.jsx";
+import WordItem from "./WordItem.jsx";
+import {useState} from "react";
+
+const App = () => {
+const [text,setText] = useState("");
+const [filterText,setFilterText] = useState("");
+  
+  const worditems = words
+    .filter((word)=>word.includes(filterText))
+    .map((word, idx) => <WordItem word={word} key={idx} />);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    
+    <div className="m-4 flex flex-col items-center">
+      <form
+        onSubmit={(e)=>{
+          e.preventDefault();
+          setFilterText(text);
+        }}
         >
-          Learn React
-        </a>
-      </header>
+        <div className="flex shadow-md m-8 rounded-lg ">
+        <button
+                onClick={()=>setText("")}
+                className="p-2 bg-green-200 px-2" >Reset</button>
+      <input
+        onChange={(e)=>setText(e.target.value)}
+        value={text}
+        className=" bg-neutral-200 p-2 " 
+        placeholder="filter words"  
+        type="text"
+        />
+          
+        <button className="p-2 bg-green-200 px-2" >Filter</button>
+        </div>
+        
+        
+        
+      </form>
+      <div className="flex flex-wrap justify-center w-full max-w-lg">
+        {worditems}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
